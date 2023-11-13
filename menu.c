d62689508ac9546884ef17c18a2fae5cf15774e1
zagozagoo <vivizago4884@gmail.com>
2023-11-13 10:50:16 -0300

    dados para nova tabela

---
+++ menu.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  FILE* file;
+  char line[200];
+  char date[20];
+  int year, month, day;
+
+  system("color 3E");
+
+  file = fopen("weather_data.txt", "r");
+
+  if (file == NULL)
+  {
+    printf("Erro ao abrir o arquivo de dados meteorológicos.\n");
+    return 1;
+  }
+
+  printf("█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n");
+  printf("█             _                 _           █\n");
+  printf("             | |               | |           \n");
+  printf("         ___ | | ___  _   _  __| |___        \n");
+  printf("        / __|| |/ _ \\| | | |/ _` / __|      \n");
+  printf("       | (__ | | (_) | |_| | (_| \\__ \\     \n");
+  printf("        \\___||_|\\___/ \\__,_|\\__,_|___/   \n");
+  printf("█                                           █\n");
+  printf("█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n");
+
+  printf("\n\n");
+  printf("☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ \n");
+  printf("Welcome to 𝐂𝐋𝐎𝐔𝐃𝐒! The system about forecasts and temperature\n");
+  printf("☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ \n");
+  printf("\n\nPlease enter year, month and day respectively to view the data:\n");
+  scanf("%d %d %d", &year, &month, &day);
+  printf("\nForecast for the date %d-%02d-%02d:\n", year, month, day);
+
+  // Imprime o cabeçalho da tabela
+  printf("\n");
+  printf("| Date                | Temperature | Humidity | Pressure |\n");
+  printf("|---------------------|-------------|----------|----------|\n");
+
+  // le as linhas e formata
+  while (fgets(line, sizeof(line), file) != NULL)
+  {
+    if (sscanf(line, "%*s %10s", date) == 1)
+    {
+      int line_year, line_month, line_day;
+      if (sscanf(date, "%04d-%02d-%02d", &line_year, &line_month, &line_day) == 3)
+      {
+        if (line_year == year && line_month == month && line_day == day)
+        {
+          char print_date[11], print_hour[9];
+          float temperature;
+          int humidity, pressure;
+
+          // extrai os valores de temperatura umidade e pressao da linha
+          sscanf(line, "Data %10s %8s;Temperatura:%f;Humidade:%d;Pressao:%d", print_date, print_hour, &temperature, &humidity, &pressure);
+
+          // imprime os valores formatados em uma linha de tabela
+          printf("| %s %s | %-11.2f | %-8d | %-7d |\n", print_date, print_hour, temperature, humidity, pressure);
+        }
+      }
+    }
+  }
+
+  fclose(file);
+  return 0;
+}
---
+++ weatherapi.py
@@ -0,0 +1,42 @@
+import requests, json
+
+api_key = "61f671689e4cb0c68eafe8a163c7ebdd"
+
+city_name = input("Enter city name : ")
+
+# Link da API que pega as coordenadas geográficas do local especificado (Documentação: https://openweathermap.org/api/geocoding-api)
+geocoding = "http://api.openweathermap.org/geo/1.0/direct?q=" + city_name + "&limit=1&appid=" + api_key
+georesponse = requests.get(geocoding) # Manda uma requisição para a API
+geojson = georesponse.json()[0] # Salva a resposta da API como json em uma variável
+
+# Link que pega a previsão do tempo do local especificado
+base_url = "https://api.openweathermap.org/data/2.5/forecast?lat="
+
+# Concatena todas as informações em um link(as informações do json estão sendo convertidas 
+# para string para poderem ser concatenadas)
+histweather = base_url + str(geojson["lat"]) + "&lon=" + str(geojson["lon"]) + "&appid=" + api_key + "&units=metric"
+histresponse = requests.get(histweather)
+histjson = histresponse.json()
+
+with open ("weather_data.txt", "w") as file:
+
+# Percorre a lista dentro do json que possui todas as informações de previsão dos proximos 5 dias
+    for i in range(len(histjson["list"])): # :)
+        list_data = histjson["list"][i] # Valor atual da lista
+        main_info = list_data["main"] # Pegando os dados principais do valor atual
+        
+        # Salvando as informações principais do valor atual da lista em uma vari
+
+        file.write(f"Data:{list_data['dt_txt']};") # Printando a data exata daquelas informações
+        file.write(f"Temperatura:{main_info['temp']};")
+        file.write(f"Humidade:{main_info['humidity']};")
+        file.write(f"Pressao:{main_info['pressure']};")
+        file.write(f"Nivel do mar:{main_info['sea_level']};")
+
+        list_data = histjson["list"][i]
+        weather_info = list_data["weather"]
+
+        for j in range(len(weather_info)):
+            file.write(f"Clima:{weather_info[j]['description']}\n")
+
+print("Dados metereologicos foram salvos no arquivo 'weather_data.txt'")
