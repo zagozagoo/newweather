20673c6ff0ec0cb04efd1884a7216dac6add3db7
Zago! <144025196+zagozagoo@users.noreply.github.com>
2023-11-13 13:51:09 -0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
-  FILE* file;
+int main() {
+  FILE *file;
   char line[200];
   char date[20];
   int year, month, day;
 
-  system("color 3E");
+  printf("\033[38;5;68m");
 
   file = fopen("weather_data.txt", "r");
 
-  if (file == NULL)
-  {
+  if (file == NULL) {
     printf("Erro ao abrir o arquivo de dados meteorológicos.\n");
     return 1;
   }
@@ -32,7 +30,8 @@ int main()
   printf("☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ \n");
   printf("Welcome to 𝐂𝐋𝐎𝐔𝐃𝐒! The system about forecasts and temperature\n");
   printf("☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ \n");
-  printf("\n\nPlease enter year, month and day respectively to view the data:\n");
+  printf(
+      "\n\nPlease enter year, month and day respectively to view the data:\n");
   scanf("%d %d %d", &year, &month, &day);
   printf("\nForecast for the date %d-%02d-%02d:\n", year, month, day);
 
@@ -42,24 +41,55 @@ int main()
   printf("|---------------------|-------------|----------|----------|\n");
 
   // le as linhas e formata
-  while (fgets(line, sizeof(line), file) != NULL)
-  {
-    if (sscanf(line, "%*s %10s", date) == 1)
-    {
+  while (fgets(line, sizeof(line), file) != NULL) {
+    if (sscanf(line, "%*s %10s", date) == 1) {
       int line_year, line_month, line_day;
-      if (sscanf(date, "%04d-%02d-%02d", &line_year, &line_month, &line_day) == 3)
-      {
-        if (line_year == year && line_month == month && line_day == day)
-        {
+      if (sscanf(date, "%04d-%02d-%02d", &line_year, &line_month, &line_day) ==
+          3) {
+        if (line_year == year && line_month == month && line_day == day) {
           char print_date[11], print_hour[9];
           float temperature;
           int humidity, pressure;
 
           // extrai os valores de temperatura umidade e pressao da linha
-          sscanf(line, "Data %10s %8s;Temperatura:%f;Humidade:%d;Pressao:%d", print_date, print_hour, &temperature, &humidity, &pressure);
+          sscanf(line, "Data %10s %8s;Temperatura:%f;Humidade:%d;Pressao:%d",
+                 print_date, print_hour, &temperature, &humidity, &pressure);
 
           // imprime os valores formatados em uma linha de tabela
-          printf("| %s %s | %-11.2f | %-8d | %-7d |\n", print_date, print_hour, temperature, humidity, pressure);
+          printf("| %s %s | %-11.2f | %-8d |  %-7d |\n", print_date, print_hour,
+                 temperature, humidity, pressure);
+        }
+      }
+    }
+  }
+  fclose(file);
+
+  file = fopen("weather_description.txt", "r");
+
+  if (file == NULL) {
+    printf("Erro ao abrir o arquivo de dados meteorológicos.\n");
+    return 1;
+  }
+
+  printf("\n");
+  printf("| Date                | Weather          |\n");
+  printf("|---------------------|------------------|\n");
+
+  // le as linhas e formata
+  while (fgets(line, sizeof(line), file) != NULL) {
+    if (sscanf(line, "%*s %10s", date) == 1) {
+      int line_year, line_month, line_day;
+      if (sscanf(date, "%04d-%02d-%02d", &line_year, &line_month, &line_day) ==
+          3) {
+        if (line_year == year && line_month == month && line_day == day) {
+          char print_date[11], print_hour[9];
+          char weather[21];
+          // extrai os valores de temperatura umidade e pressao da linha
+          sscanf(line, "Data %10s %8s;Weather:%[^;]", print_date, print_hour,
+                 weather);
+
+          // imprime os valores formatados em uma linha de tabela
+          printf("| %s %s | %s \n", print_date, print_hour, weather);
         }
       }
     }
